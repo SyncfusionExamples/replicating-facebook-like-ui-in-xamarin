515a0b73c0e598a2434be965ec6a2e53ef5d388b
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-04-15 15:43:03 +0530

    Added sample

---
+++ FB_Like_Sample/FB_Like_Sample.iOS/AppDelegate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using Syncfusion.ListView.XForms.iOS;
+using Syncfusion.SfRating.XForms.iOS;
+using Syncfusion.XForms.iOS.BadgeView;
+using Syncfusion.XForms.iOS.Border;
+using Syncfusion.XForms.iOS.Buttons;
+using Syncfusion.XForms.iOS.Cards;
+using Syncfusion.XForms.iOS.Core;
+using Syncfusion.XForms.iOS.Graphics;
+using Syncfusion.XForms.iOS.PopupLayout;
+using UIKit;
+
+namespace FB_Like_Sample.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            SfButtonRenderer.Init();
+            SfCheckBoxRenderer.Init();
+            SfBorderRenderer.Init();
+            SfGradientViewRenderer.Init();
+            SfListViewRenderer.Init();
+            SfRatingRenderer.Init();
+            SfRadioButtonRenderer.Init();
+            SfPopupLayoutRenderer.Init();
+            SfCardViewRenderer.Init();
+            SfBadgeViewRenderer.Init();
+            SfSegmentedControlRenderer.Init();
+            Core.Init();
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ FB_Like_Sample/FB_Like_Sample.iOS/DependencyService/iOSKeyboardHelper .cs
@@ -0,0 +1,20 @@
+﻿using FB_Like_Sample.Interface;
+using FB_Like_Sample.iOS.DependencyService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(iOSKeyboardHelper))]
+namespace FB_Like_Sample.iOS.DependencyService
+{
+    public class iOSKeyboardHelper : IKeyboardHelper
+    {
+        public void HideKeyboard()
+        {
+            UIApplication.SharedApplication.KeyWindow.EndEditing(true);
+        }
+    }
+}
